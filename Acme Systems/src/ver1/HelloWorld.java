9e3ba05bbe52685af25ed5d370e3c1b1538e4754
Jason Dania <jmdania@valdosta.edu>
2019-09-05 00:23:21 -0400

    Added "Hey Now!" print statement

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -3,9 +3,10 @@ package ver1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    System.out.println("Hello from Eclipse");
-    System.out.println("Hello from GitHub");	
+	    String Msg = "Hey Now!";
+	    
+		System.out.println(Msg);
+        System.out.println("Hello from Eclipse");
+        System.out.println("Hello from GitHub");	
 	}
-
 }
